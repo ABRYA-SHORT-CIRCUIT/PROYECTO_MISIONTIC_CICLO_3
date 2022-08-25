dec62bc520e7098adb506b8feebad20092f8c227
YoryinaOrtegaUparela <109432483+YoryinaOrtegaUparela@users.noreply.github.com>
2022-08-25 10:38:14 -0500

    Resumen
    Se parametriza properties para la base de datos y se excluye de momento la autenticación.

--- src/main/java/com/proyecto/tvshop/TvshopApplication.java
+++ src/main/java/com/proyecto/tvshop/TvshopApplication.java
@@ -2,8 +2,9 @@ package com.proyecto.tvshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude ={SecurityAutoConfiguration.class})
 public class TvshopApplication {
 
 	public static void main(String[] args) {
