d644ab528d26c5dc1022b8fd2310b795838bcf8f
YoryinaOrtegaUparela <109432483+YoryinaOrtegaUparela@users.noreply.github.com>
2022-08-25 10:28:12 -0500

    Creación inicial del proyecto

---
+++ src/main/java/com/proyecto/tvshop/TvshopApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.tvshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TvshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TvshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyecto/tvshop/TvshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.tvshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TvshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
