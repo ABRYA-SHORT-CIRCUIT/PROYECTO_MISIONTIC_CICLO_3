b9624e3cc75dffd9caef0ae3b23e0df42cdf606c
YoryinaOrtegaUparela <109432483+YoryinaOrtegaUparela@users.noreply.github.com>
2022-09-03 10:54:38 -0500

    Creacion De controller para Usuario y Empresa
    Creacion De controller para Usuario y Empresa

--- src/main/java/com/proyecto/tvshop/restController/EmpresaController.java
+++ src/main/java/com/proyecto/tvshop/restController/EmpresaController.java
@@ -1,7 +1,6 @@
 package com.proyecto.tvshop.restController;
 
 import com.proyecto.tvshop.modelos.Empresa;
-import com.proyecto.tvshop.servicios.EmpresaServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,32 +9,32 @@ import java.util.List;
 @RestController
 public class EmpresaController {
 
-    @Autowired
-    EmpresaServices empresaServices;
+//    @Autowired
+//    EmpresaServices empresaServices;
 
     @GetMapping("/enterprises")
     public List<Empresa> consultarTodasempresas(){
-        return empresaServices.consultarTodasempresas();
+        return null;
     }
 
     @PostMapping("/enterprises")
     public Empresa crearEmpresa(@RequestBody Empresa empresaNueva){
-        return empresaServices.crearEmpresa(empresaNueva);
+        return null;
     }
 
     @GetMapping("/enterprises/{id}")
     public Empresa consultarEmpresa(@PathVariable("id") Integer id_empresa){
-        return empresaServices.consultarEmpresa(id_empresa);
+        return null;
     }
 
     @PatchMapping("/enterprises/{id}")
     public Empresa actualizarEmpresa(@RequestBody Empresa empresa){
-        return empresaServices.editarEmpresa(empresa);
+        return null;
     }
 
     @DeleteMapping("/enterprises/{id}")
     public String eliminarEmpresa(@PathVariable("id") Integer idEmpresa){
-        return empresaServices.eliminarEmpresa(idEmpresa);
+        return null;
     }
 
 }
--- src/main/java/com/proyecto/tvshop/restController/UsuarioController.java
+++ src/main/java/com/proyecto/tvshop/restController/UsuarioController.java
@@ -1,8 +1,6 @@
 package com.proyecto.tvshop.restController;
 
-import com.proyecto.tvshop.modelos.Empresa;
 import com.proyecto.tvshop.modelos.Usuario;
-import com.proyecto.tvshop.servicios.UsuarioServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,31 +9,34 @@ import java.util.List;
 @RestController
 public class UsuarioController {
 
-    @Autowired
-    UsuarioServices usuarioServices;
+//    @Autowired
+//    UsuarioServices usuarioServices;
 
     @GetMapping("/users")
     public List<Usuario> consultarTodosUsuarios() {
-        return usuarioServices.consultarTodosUsuarios();
+
+        return null;
     }
 
     @PostMapping("/users")
     public Usuario crearUsuario(@RequestBody Usuario usuarioNuevo){
-        return usuarioServices.crearUsuario(usuarioNuevo);
+        return null;
     }
 
     @GetMapping("/user/{id}")
     public Usuario consultarUsuario(@PathVariable("id") Integer id_usuario){
-        return usuarioServices.consultarUsuario(id_usuario);
+        return null;
     }
 
     @PatchMapping("/user/{id}")
     public Usuario editarUsuario(@RequestBody Usuario usuario){
-        return usuarioServices.editarUsuario(usuario);
+
+         return null;
     }
 
     @DeleteMapping("/user/{id}")
     public String eliminarUsuario(@PathVariable("id") Integer id){
-        return usuarioServices.eliminarUsuario(id);
+
+        return null;
     }
 }
