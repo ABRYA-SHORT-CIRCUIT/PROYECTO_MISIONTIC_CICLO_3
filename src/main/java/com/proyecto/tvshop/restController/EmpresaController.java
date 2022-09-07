cd2126476449e95464462c846077779ca7815a75
Ana Correa <105757028+anamcb90@users.noreply.github.com>
2022-09-07 18:16:28 -0500

    Update EmpresaController.java
    Cambios de Alex

--- src/main/java/com/proyecto/tvshop/restController/EmpresaController.java
+++ src/main/java/com/proyecto/tvshop/restController/EmpresaController.java
@@ -14,44 +14,31 @@ public class EmpresaController {
     private EmpresaService empresaService;
 
     @GetMapping("/enterprises")
-    public List<Empresa> consultarTodasempresas(){
-
+    public List<Empresa> showAllEnterprises(){
         return empresaService.listarEmpresas();
     }
 
     @PostMapping("/enterprises")
-    public Empresa crearEmpresa(@RequestBody Empresa empresaNueva){
-        return empresaService.crearEmpresa(empresaNueva);
+    public Empresa addEnterprise(@RequestBody Empresa newEnterprise){
+        return empresaService.crearEmpresa(newEnterprise);
     }
 
     @GetMapping("/enterprises/{id}")
-    public Empresa consultarEmpresa(@PathVariable("id") Integer id_empresa){
-        return empresaService.consultarEmpresa(id_empresa);
+    public Optional<Empresa> showEnterprise(@PathVariable("id") Integer idEnterprise){
+        return empresaService.consultarEmpresa(idEnterprise);
     }
 
-    //aca habia un error, no estaba usando la anotacion @Pathvariable para tomar el id de la empresa a actualizar
-    //tampoco se estaban seteando los datos de la empresa editada con los nuevos datos ingresados
     @PatchMapping("/enterprises/{id}")
-    public Empresa actualizarEmpresa(@PathVariable("id") Integer id, @RequestBody Empresa empresa){
-        Empresa empresaEditada= empresaService.consultarEmpresa(id);
-        empresaEditada.setNombre(empresa.getNombre());
-        empresaEditada.setDireccion(empresa.getDireccion());
-        empresaEditada.setTelefono(empresa.getTelefono());
-        empresaEditada.setNit(empresa.getNit());
-        return empresaService.crearEmpresa(empresa);
+    public Empresa updateEnterprise(@RequestBody Empresa editedEnterprise){
+        return empresaService.crearEmpresa(editedEnterprise);
     }
 
-    //Corregí el mensaje del return
-    //no me funciona el DELETE en el postman
     @DeleteMapping("/enterprises/{id}")
-    public String eliminarEmpresa(@PathVariable("id") Integer idEmpresa){
-        boolean response = empresaService.eliminarEmpresa(idEmpresa);
-        if(response) {
-            return "Empresa eliminada correctamente";
+    public String deleteEnterprise(@PathVariable("id") Integer idEnterprise){
+        if(empresaService.eliminarEmpresa(idEnterprise)) {
+            return "La empresa fue eliminada correctamente";
         }
-        else{
-            return "La empresa no fue eliminada";
-    }
+        return "La empresa no fue eliminada";
     }
 
 }
